2decec4e708444557ea061f2ac34be6da1f77ac5
Reza Primasatya <rezaprimasatya@gmail.com>
2018-01-17 00:42:49 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+import time
+import webbrowser
+
+for i in range(3):
+    print "Count Down timer = " + time.ctime()
+    time.sleep(3)
+    webbrowser.open("https://youtu.be/BFjyhvJ-q7o")
+
+# i = 0
+# while i < 3:
+    # print "Count Down timer = " + time.ctime()
+#     time.sleep(3)
+#     webbrowser.open("https://youtu.be/BFjyhvJ-q7o")
+#     i = i + 1
